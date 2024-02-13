7bd5a5d070a96d44802fcdb9f813a53aba72ff39
Lina Vela <u20232217336@usco.edu.co>
2024-02-12 20:34:14 -0500

    ahora el intex.html tiene codigo html

--- app.js
+++ app.js
@@ -1,6 +1,1 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-
-
+console.log('Hola mundo');
